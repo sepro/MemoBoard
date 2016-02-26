edc138776a12cce64a851ca569f255ea846e7884
sepro <sebastian.proost@gmail.com>
2016-02-26 15:12:30 +0100

    datetime default

--- memoboard/models.py
+++ memoboard/models.py
@@ -6,14 +6,12 @@ class MemoList(db.Model):
     __tablename__ = 'lists'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.Text)
-    added = db.Column(db.DateTime)
+    added = db.Column(db.DateTime, default=datetime.utcnow)
 
     items = db.relationship('MemoItem', backref=db.backref('list', lazy='joined'), lazy='dynamic')
 
-    def __init__(self, name,
-                 added=datetime.now().replace(microsecond=0)):
+    def __init__(self, name):
         self.name = name
-        self.added = added
 
     def __repr__(self):
         return '<MemoList %d>' % self.id
@@ -23,14 +21,13 @@ class MemoItem(db.Model):
     __tablename__ = 'list_items'
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.Text)
-    added = db.Column(db.DateTime)
+    added = db.Column(db.DateTime, default=datetime.utcnow)
 
     list_id = db.Column(db.Integer, db.ForeignKey('lists.id'), index=True)
 
-    def __init__(self, content,
-                 added=datetime.now().replace(microsecond=0)):
+    def __init__(self, content):
         self.content = content
-        self.added = added
 
     def __repr__(self):
         return '<MemoItem %d>' % self.id
+
