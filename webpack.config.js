91a8c863ef0a065377af3b1607886fc53f7a4f23
Sebastian Proost <sebastian.proost@gmail.com>
2016-03-02 20:36:46 +0100

    set up react stuff

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  entry: './jsx/main.js',
+  output: { path: './memoboard/static/js/', filename: 'bundle.js' },
+  module: {
+    loaders: [
+      {
+        test: /.jsx?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  },
+};
